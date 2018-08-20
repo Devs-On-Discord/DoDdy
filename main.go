13ff522f28788c09e30a48a096f3d334cbc8a7e3
Miyoyo <miyoyo@live.be>
2018-08-21 01:40:51 +0200

    Basic bot structure

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-	fmt.Println("Hey!" + token)
+	bot, err := discordgo.New("Bot " + testToken)
+	if err != nil {
+		panic(err.Error())
+	}
+	if bot.Open() != nil {
+		panic("could not open bot: " + err.Error())
+	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	bot.Close()
 }
