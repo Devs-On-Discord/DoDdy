e8fac708d5d6da77bf94bdcbcd2b1ea3a4744db2
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-09-02 18:03:25 +0200

    Add reaction vote decrease

--- votes/votes.go
+++ votes/votes.go
@@ -20,6 +20,7 @@ type Votes struct {
 func (v *Votes) Init(session *discordgo.Session) {
 	v.session = session
 	v.session.AddHandler(v.reactionAdded)
+	v.session.AddHandler(v.reactionRemoved)
 	v.Votes = make(map[string]Vote, 0)
 	v.Load()
 }
@@ -52,6 +53,18 @@ func (v *Votes) reactionAdded(session *discordgo.Session, reaction *discordgo.Me
 		for _, answer := range vote.Answers {
 			if answer.emojiID == reaction.Emoji.ID {
 				IncreaseVoteAnswer(vote.Id, answer.emojiID)
+				break
+			}
+		}
+		session.MessageReactionsRemoveAll(reaction.ChannelID, reaction.MessageID)
+	}
+}
+
+func (v *Votes) reactionRemoved(session *discordgo.Session, reaction *discordgo.MessageReactionRemove) {
+	if vote, exists := v.Votes[reaction.ChannelID]; exists {
+		for _, answer := range vote.Answers {
+			if answer.emojiID == reaction.Emoji.ID {
+				DecreaseVoteAnswer(vote.Id, answer.emojiID)
 				break
 			}
 		}
