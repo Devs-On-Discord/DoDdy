490e1ca4bb8c24ffffae0ece0a4cb7d18e7db4bb
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-09-02 19:15:36 +0200

    Send help as dm

--- botCommands/helpCommand.go
+++ botCommands/helpCommand.go
@@ -4,15 +4,30 @@ import "github.com/bwmarrin/discordgo"
 import "github.com/Devs-On-Discord/DoDdy/commands"
 
 func helpCommand(session *discordgo.Session, commandMessage *discordgo.MessageCreate, args []string) commands.CommandResultMessage {
+	helpText := "setup\n" +
+		"setAnnouncementsChannel\n" +
+		"postAnnouncement\n" +
+		"postLastMessageAsAnnouncement\n" +
+		"clearAnnouncements\n" +
+		"setVotesChannel\n" +
+		"vote\n" +
+		"prefix"
+	userChannel, err := session.UserChannelCreate(commandMessage.Author.ID)
+	if err != nil {
+		return &commands.CommandError{
+			Message: "Bot couldn't contact you " + err.Error(),
+			Color: 0xb30000,
+		}
+	}
+	_, err = session.ChannelMessageSend(userChannel.ID, helpText)
+	if err != nil {
+		return &commands.CommandError{
+			Message: "Help couldn't be send as an dm " + err.Error(),
+			Color: 0xb30000,
+		}
+	}
 	return &commands.CommandReply{
-		Message: "setup\n" +
-			"setAnnouncementsChannel\n" +
-			"postAnnouncement\n" +
-			"postLastMessageAsAnnouncement\n" +
-			"clearAnnouncements\n" +
-			"setVotesChannel\n" +
-			"vote\n" +
-			"prefix",
+		Message: "Help has been send as dm",
 		Color: 0x00b300,
 	}
 }
