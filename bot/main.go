500b60b76d8353aed92dc2178320068847307700
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-09-06 13:08:28 +0200

    vote test in main

--- bot/main.go
+++ bot/main.go
@@ -29,12 +29,21 @@ func main() {
 	v := &Votes{}
 	v.Init(db.db, bot.session)
 
+	v2 := &votes2{}
+	v2.Init()
+	println("votes2 len", len(v2.entities))
+	for _, entity := range v2.entities {
+		entityOBj := *entity
+		entVote := entityOBj.(*vote)
+		println("name", entVote.GetString("name"))
+	}
+
 	botCommands := &commands.Commands{}
 	botCommands.Init(bot.session)
 	botCommands.Validator = commandValidator{guilds: g}
 	botCommands.Identifier = commandIdentifier{guilds: g}
 	botCommands.ResultHandler = commandResultHandler{}
-	botCommands.RegisterGroup(guildAdminCommands{guilds: g, votes: v})
+	botCommands.RegisterGroup(guildAdminCommands{guilds: g, votes: v, votes2: v2})
 	botCommands.RegisterGroup(helpCommands{botCommands})
 
 	sc := make(chan os.Signal, 1)
