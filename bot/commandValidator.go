d6f1fd6b86874fdf4b6b9cfd2387db7807ce7520
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-09-09 16:27:45 +0200

    Cleanup command validator

--- bot/commandValidator.go
+++ bot/commandValidator.go
@@ -14,16 +14,13 @@ func (v commandValidator) Validate(command *commands.Command, session *discordgo
 	if err != nil {
 		return false
 	}
-	guildPtr, err := v.guilds.Entity(message.GuildID)
+	guild, err := v.guilds.Guild(message.GuildID)
 	if err != nil {
 		//TODO: check if member role is an high one to accept the first commands without setting the roles, for example !setup
 		return false
 	}
-	guild := *guildPtr
-	if rawRoles, err := guild.Get("roles"); err == nil {
-		roles := rawRoles.(map[Role]string)
-		commandRole := RoleInt[command.Role]
-		for role, id := range roles {
+	if commandRole, exists := RoleInt[command.Role]; exists {
+		for role, id := range guild.roles {
 			for _, memberRole := range member.Roles {
 				if id == memberRole {
 					if role >= commandRole {
