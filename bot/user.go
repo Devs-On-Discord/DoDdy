dcebe35c989fb5617ca8c317d8e02bff946c79f8
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-09-26 20:41:39 +0200

    add user entity

--- bot/user.go
+++ bot/user.go
@@ -1,27 +1,57 @@
 package main
 
-/*import "fmt"
+import (
+	"encoding/binary"
+	bolt "go.etcd.io/bbolt"
+)
 
-func (u *user) Insert(usersBucket *bolt.Bucket) error {
-	userBucket, err := usersBucket.CreateBucket([]byte(u.id))
-	if err != nil {
-		return fmt.Errorf("create user: %s", err)
+type user struct {
+	entity
+	id   string
+	reputation uint64
+}
+
+func (u *user) Init() {
+	u.entity.Init()
+	u.fields = map[string]*entityField{
+		"id": {
+			setter: func(val interface{}) {
+				if id, ok := val.(string); ok {
+					u.id = id
+				}
+			},
+			getter: func() interface{} {
+				return u.id
+			},
+		},
+		"reputation": {
+			setter: func(val interface{}) {
+				if reputation, ok := val.(uint64); ok {
+					u.reputation = reputation
+				}
+			},
+			getter: func() interface{} {
+				return u.reputation
+			},
+		},
 	}
-	err = userBucket.Put([]byte("id"), []byte(string(u.id)))
-	return err
+	u.name = "user"
+	u.onLoad = u.OnLoad
+	u.onSave = u.OnSave
+
+	u.reputation = 0
 }
 
-func (u *user) Delete() {
-	db.DB.Update(func(tx *bolt.Tx) error {
-		usersBucket := tx.Bucket([]byte("users"))
-		if usersBucket == nil {
-			return nil
-		}
-		err := usersBucket.DeleteBucket([]byte(u.id))
-		if err != nil {
-			return fmt.Errorf("delete user: %s", err)
-		}
-		return nil
-	})
+func (u *user) OnLoad(key string, val []byte, bucket *bolt.Bucket) interface{} {
+	switch key {
+	case "id":
+		return string(val)
+	case "reputation":
+		return binary.LittleEndian.Uint64(val)
+	}
+	return nil
 }
-*/
+
+func (u *user) OnSave(key string, val interface{}, bucket *bolt.Bucket) (interface{}, error) {
+	return nil, nil
+}
