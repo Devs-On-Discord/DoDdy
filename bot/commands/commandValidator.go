faf8bbb1005a81da07aa6d3a2b937fb72b15bcf0
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-10-31 12:34:08 +0100

    Initial commit

--- bot/commands/commandValidator.go
+++
@@ -1,7 +0,0 @@
-package commands
-
-import "github.com/bwmarrin/discordgo"
-
-type CommandValidator interface {
-	Validate(command *Command, session *discordgo.Session, message *discordgo.MessageCreate) bool
-}
