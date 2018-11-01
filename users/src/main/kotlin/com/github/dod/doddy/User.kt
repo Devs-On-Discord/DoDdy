6cdabce10d280bd4c82997f798d9cb04aaaf87f8
ThisIsIPat <mmf.ipat@gmail.com>
2018-11-01 03:03:18 +0100

    Finished TODO task

--- users/src/main/kotlin/com/github/dod/doddy/User.kt
+++ users/src/main/kotlin/com/github/dod/doddy/User.kt
@@ -2,6 +2,6 @@ package com.github.dod.doddy
 
 data class User(val id: String,
                 val guildReputations: Map<String, Long>) {
-    val globalReputation: Long
-    get() = guildReputations.values.sum()   // TODO: Redo formula
+    val globalReputation: Long?
+    get() = guildReputations.values.max()
 }
