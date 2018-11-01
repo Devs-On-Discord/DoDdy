24b2daf85e02ac086c4edbd60c418a43bd54bb9c
ThisIsIPat <mmf.ipat@gmail.com>
2018-11-01 02:57:59 +0100

    Added TODO element for User class

--- users/src/main/kotlin/com/github/dod/doddy/User.kt
+++ users/src/main/kotlin/com/github/dod/doddy/User.kt
@@ -2,6 +2,6 @@ package com.github.dod.doddy
 
 data class User(val id: String,
                 val guildReputations: Map<String, Long>) {
-    val globalReputation: Long // Global reputation
-    get() = guildReputations.values.sum()
+    val globalReputation: Long
+    get() = guildReputations.values.sum()   // TODO: Redo formula
 }
