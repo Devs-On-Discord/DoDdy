f8d67efc7a145f8846829aad87c6c07467ccc8c6
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-01 04:08:10 +0100

    Specify kotlin version and fix build

--- bot/src/main/kotlin/com/github/dod/doddy/Main.kt
+++ bot/src/main/kotlin/com/github/dod/doddy/Main.kt
@@ -1,3 +1,4 @@
+@file:JvmName("Main")
 package com.github.dod.doddy
 
 fun main(args : Array<String>) {
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,9 @@
 object Versions {
     val jda = "3.8.1_437"
+    val kotlin_version = "1.3.0"
 }
 
 object Libs {
     val discord_bot_sdk = "net.dv8tion:JDA:${Versions.jda}"
-    val stdlib = "org.jetbrains.kotlin:kotlin-stdlib"
+    val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin_version}"
 }
