16fb0d7a47ba31e426aefd90d1bf958516dc9083
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-01 10:01:58 +0100

    Update javadoc

--- core/src/main/kotlin/com/github/dod/doddy/core/Module.kt
+++ core/src/main/kotlin/com/github/dod/doddy/core/Module.kt
@@ -2,7 +2,7 @@ package com.github.dod.doddy.core
 
 interface Module {
     /**
-     * @return true to forward commands, false to consume
+     * @return true to consume
      */
     fun onCommand(): Boolean
 }
