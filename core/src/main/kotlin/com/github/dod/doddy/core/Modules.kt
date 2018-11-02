ea1dae1e07fa3f862f72bd4b78669cea82124e3c
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-02 07:53:27 +0100

    Reference to the parent module from an command isn't required

--- core/src/main/kotlin/com/github/dod/doddy/core/Modules.kt
+++ core/src/main/kotlin/com/github/dod/doddy/core/Modules.kt
@@ -1,19 +1,17 @@
 package com.github.dod.doddy.core
 
 object Modules {
-    private val moduleCommands = mutableMapOf<Command, Module>()
-    private val commandNames = mutableMapOf<String, Command>()
+    private val commands = mutableMapOf<String, Command>()
 
     fun add(module: Module) {
         module.getCommands().forEach { command ->
-            moduleCommands[command] = module
             command.names.forEach {name ->
-                commandNames[name] = command
+                commands[name] = command
             }
         }
     }
 
     internal fun onCommand(name: String, args: Array<Any>): Boolean {
-        return commandNames[name]?.onCommand(args) ?: false
+        return commands[name]?.onCommand(args) ?: false
     }
 }
