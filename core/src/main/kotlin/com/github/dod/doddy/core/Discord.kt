5ae4dc3d8aad105ee5391e747b5d50939afb14fa
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-03 17:42:53 +0100

    Fix message event listener

--- core/src/main/kotlin/com/github/dod/doddy/core/Discord.kt
+++ core/src/main/kotlin/com/github/dod/doddy/core/Discord.kt
@@ -2,17 +2,14 @@ package com.github.dod.doddy.core
 
 import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.JDABuilder
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent
-import net.dv8tion.jda.core.hooks.ListenerAdapter
 
-
-object Discord : ListenerAdapter() {
+object Discord {
     private val modules = Modules()
 
     val bot: JDA = JDABuilder("token").build()
 
     init {
-
+        bot.addEventListener(MessageListener(modules))
     }
 
     fun add(module: Module) {
@@ -24,24 +21,5 @@ object Discord : ListenerAdapter() {
 
     fun start() {
         bot.awaitReady()
-    }
-
-    override fun onMessageReceived(event: MessageReceivedEvent) {
-        if (event.author.isBot) return
-        val content = event.message.contentRaw
-        val length = content.length
-        if (length == 0) return
-        if (content[0] == '!') {//TODO: use from db
-            val parsedMessage = content.slice(1 until length - 1).trim().split("/ +/g")
-            val parsedMessageSize = parsedMessage.size
-            if (parsedMessageSize == 0) return
-            val commandName = parsedMessage[0]
-            val commandArgs = if (parsedMessageSize > 1) { // has args
-                parsedMessage.subList(1, parsedMessageSize - 1)
-            } else {
-                emptyList()
-            }
-            modules.onCommand(commandName, commandArgs)
-        }
     }
 }
---
+++ core/src/main/kotlin/com/github/dod/doddy/core/MessageListener.kt
@@ -0,0 +1,32 @@
+package com.github.dod.doddy.core
+
+import net.dv8tion.jda.core.events.Event
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import net.dv8tion.jda.core.hooks.EventListener
+
+class MessageListener(private val modules: Modules): EventListener {
+    override fun onEvent(event: Event) {
+        when (event) {
+            is MessageReceivedEvent -> onMessageReceived(event)
+        }
+    }
+
+    private fun onMessageReceived(event: MessageReceivedEvent) {
+        if (event.author.isBot) return
+        val content = event.message.contentRaw
+        val length = content.length
+        if (length == 0) return
+        if (content[0] == '!') {//TODO: use from db
+            val parsedMessage = content.slice(1 until length - 1).trim().split("/ +/g")
+            val parsedMessageSize = parsedMessage.size
+            if (parsedMessageSize == 0) return
+            val commandName = parsedMessage[0]
+            val commandArgs = if (parsedMessageSize > 1) { // has args
+                parsedMessage.subList(1, parsedMessageSize - 1)
+            } else {
+                emptyList()
+            }
+            modules.onCommand(commandName, commandArgs)
+        }
+    }
+}
