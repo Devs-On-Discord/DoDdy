152bfe781e09b54f5d8128041644f5b7ad93bae7
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-03 19:27:53 +0100

    Add db module functionality

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,10 +1,13 @@
 object Versions {
     val jda = "3.8.1_439"
-    val kotlin_version = "1.3.0"
+    val kotlin = "1.3.0"
+    val mongodb = "3.8.3"
 }
 
 object Libs {
     val discord_bot_sdk = "net.dv8tion:JDA:${Versions.jda}"
-    val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin_version}"
-    val reflection = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin_version}"
+    val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    val reflection = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
+    val mongodb = "org.litote.kmongo:kmongo:${Versions.mongodb}"
+    val mongodb_coroutines = "org.litote.kmongo:kmongo-coroutine:${Versions.mongodb}"
 }
