739e8fbe59c3aa44621aea7692a0bfb9bee23356
MrWasdennnoch <leiter04@gmail.com>
2018-11-04 22:38:49 +0100

    Add support for member argument type and add almost empty bans module

---
+++ bans/README.md
@@ -0,0 +1,28 @@
+# Bans Module
+
+| Feature         | Author          |
+| --------------- |:---------------:|
+| `!ban`         | @MrWasdennnoch  |
+| _documentation_ | @MrWasdennnoch  |
+| _testing_       | @MrWasdennnoch  |
+
+## !ban
+
+### Description
+_Bans the specified member._
+
+Bans the specified member and provides an option for other node mods to also
+ban the user in their nodes.
+
+#### Usage:
+```
+!ban <member>
+```
+
+#### Example output:
+```
+TODO("Base implementation")
+```
+
+#### Aliases:
+* ban
--- core/src/main/kotlin/com/github/dod/doddy/core/CommandFunction.kt
+++ core/src/main/kotlin/com/github/dod/doddy/core/CommandFunction.kt
@@ -1,5 +1,6 @@
 package com.github.dod.doddy.core
 
+import net.dv8tion.jda.core.entities.Member
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import java.lang.Exception
 import kotlin.reflect.KFunction
@@ -16,11 +17,16 @@ data class CommandFunction(
 ) {
 
     companion object {
+        private val snowflakeRegex = Regex("\\d{17,19}")
+        private val mentionRegex = Regex("<@!?\\d{17,19}>")
+        private val usernameDiscrimRegex = Regex(".+#\\d{4}")
+
         private val stringType = String::class.java
         private val intType = Int::class.java
         private val longType = Long::class.java
         private val shortType = Short::class.java
         private val doubleType = Double::class.java
+        private val memberType = Member::class.java
     }
 
     suspend fun call(event: MessageReceivedEvent, args: List<String>): CommandResult {
@@ -70,6 +76,31 @@ data class CommandFunction(
                             params.add(paramIndex, number)
                         } else {
                             return InvalidArg(argument, "not a number")
+                        }
+                    }
+                    memberType -> {
+                        val member: Member? = when {
+                            snowflakeRegex.matches(argument) -> event.guild.getMemberById(argument)
+                            mentionRegex.matches(argument) -> event.guild.getMemberById(argument.slice(2 until argument.length - 1).removePrefix("!"))
+                            usernameDiscrimRegex.matches(argument) -> {
+                                val hashIndex = argument.lastIndexOf("#")
+                                val username = argument.slice(0 until hashIndex)
+                                val discrim = argument.substring(hashIndex + 1)
+                                val mem = event.guild.getMembersByName(username, true).filter {
+                                    it.user.discriminator == discrim
+                                }
+                                mem.getOrNull(0)
+                            }
+                            else -> {
+                                val mem1 = event.guild.getMembersByNickname(argument, true)
+                                val mem2 = event.guild.getMembersByName(argument, true)
+                                mem1.getOrNull(0) ?: mem2.getOrNull(0)
+                            }
+                        }
+                        if (member != null) {
+                            params.add(paramIndex, member)
+                        } else {
+                            return InvalidArg(argument, "not a known member in this guild")
                         }
                     }
                 }
